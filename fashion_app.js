544c7d8a2de00cc2fe9b48334c637137e27973fc
OkolieGold <139628934+OkolieGold@users.noreply.github.com>
2023-11-25 17:00:04 +0100

    Uploaded a new folder with multiple files

---
+++ fashion_app.js
@@ -0,0 +1,383 @@
+function palazo(){
+    if(document.getElementById("a").innerHTML === "Palazo"){
+        document.getElementById("a").innerHTML = "#5,000.00 (Five Thousand Naira)";
+    } else {
+        document.getElementById("a").innerHTML = "Palazo";
+    };
+};
+
+function jumpsuit(){
+    if(document.getElementById("b").innerHTML === "Jump Suit"){
+        document.getElementById("b").innerHTML = "#10,000.00 (Ten Thousand Naira)";
+    } else {
+        document.getElementById("b").innerHTML = "Jump Suit";
+    };
+};
+
+function kimono(){
+    if(document.getElementById("c").innerHTML === "Kimono Jacket"){
+        document.getElementById("c").innerHTML = "#10,000.00 (Ten Thousand Naira)";
+    } else {
+        document.getElementById("c").innerHTML = "Kimono Jacket";
+    };
+};
+
+function power(){
+    if(document.getElementById("d").innerHTML === "Power Suit"){
+        document.getElementById("d").innerHTML = "#20,000.00 (Twenty Thousand Naira)";
+    } else {
+        document.getElementById("d").innerHTML = "Power Suit";
+    };
+};
+
+function blouse(){
+    if(document.getElementById("e").innerHTML === "Top"){
+        document.getElementById("e").innerHTML = "#5,000.00 (Five Thousand Naira)";
+    } else {
+    document.getElementById("e").innerHTML = "Top";
+    };
+};
+
+function leatherpant(){
+    if(document.getElementById("f").innerHTML === "Leather Pant"){
+        document.getElementById("f").innerHTML = "#15,000.00 (Fifteen Thousand Naira)";
+    } else {
+        document.getElementById("f").innerHTML = "Leather Pant";
+    };
+};
+
+function rip(){
+    if(document.getElementById("aj").innerHTML === "Ripped Casual Jean"){
+        document.getElementById("aj").innerHTML = "#10,000.00 (Ten Thousand Naira)";
+    } else {
+        document.getElementById("aj").innerHTML = "Ripped Casual Jean";
+    };
+};
+
+function skinny(){
+    if(document.getElementById("ai").innerHTML === "Skinny Waist Jean"){
+        document.getElementById("ai").innerHTML = "#8,000.00 (Eight Thousand Naira)";
+    } else {
+        document.getElementById("ai").innerHTML = "Skinny Waist Jean";
+    };
+};
+
+function jacket(){
+    if(document.getElementById("ah").innerHTML === "Casual Jean Jacket"){
+        document.getElementById("ah").innerHTML = "#15,000.00 (Fifteen Thousand Naira)";
+    } else {
+        document.getElementById("ah").innerHTML = "Casual Jean Jacket";
+    };
+};
+
+function short(){
+    if(document.getElementById("ak").innerHTML === "V Neck Short Gown"){
+        document.getElementById("ak").innerHTML = "#25,000.00 (Twenty Five Thousand Naira)";
+    } else {
+        document.getElementById("ak").innerHTML = "V Neck Short Gown";
+    };
+};
+
+function button(){
+    if(document.getElementById("al").innerHTML === "Summer Button Wrap Skirt"){
+        document.getElementById("al").innerHTML = "#12,000.00 (Twelve Thousand Naira)";
+    } else {
+        document.getElementById("al").innerHTML = "Summer Button Wrap Skirt";
+    };
+};
+
+function slit(){
+    if(document.getElementById("am").innerHTML === "Ruched Ruffle Slit Skirt"){
+        document.getElementById("am").innerHTML = "#15,000.00 (Fifteen Thousand Naira)";
+    } else {
+        document.getElementById("am").innerHTML = "Ruched Ruffle Slit Skirt";
+    };
+};
+
+function gold(){
+    if(document.getElementById("g").innerHTML === "Bling Bow Waist Chain"){
+        document.getElementById("g").innerHTML = "#30,000.00 (Thirty Thousand Naira)";
+    } else {
+        document.getElementById("g").innerHTML = "Bling Bow Waist Chain";
+    };
+};
+
+function silver(){
+    if(document.getElementById("h").innerHTML === "Silver Double Heart Ring"){
+        document.getElementById("h").innerHTML = "#40,000.00 (Forty Thousand Naira)";
+    } else {
+        document.getElementById("h").innerHTML = "Silver Double Heart Ring";
+    };
+};
+
+function hat(){
+    if(document.getElementById("i").innerHTML === "Straw Bow Ribbon Hat"){
+        document.getElementById("i").innerHTML = "#10,000.00 (Ten Thousand Naira)";
+    } else {
+        document.getElementById("i").innerHTML = "Straw Bow Ribbon Hat";
+    };
+};
+
+function belt(){
+    if(document.getElementById("j").innerHTML === "Metal Leaf Buckle Belt"){
+        document.getElementById("j").innerHTML = "#10,000.00 (Ten Thousand Naira)";
+    } else {
+        document.getElementById("j").innerHTML = "Metal Leaf Buckle Belt";
+    };
+};
+
+function watch(){
+    if(document.getElementById("k").innerHTML === "BFF Wrist Watch"){
+        document.getElementById("k").innerHTML = "#25,000.00 (Twenty-Five Thousand Naira)";
+    } else {
+        document.getElementById("k").innerHTML = "BFF Wrist Watch";
+    };
+};
+
+function bracelet(){
+    if(document.getElementById("l").innerHTML === "Crystal Beaded Bracelet"){
+        document.getElementById("l").innerHTML = "#25,000.00 (Twenty-Five Thousand Naira)";
+    } else {
+        document.getElementById("l").innerHTML = "Crystal Beaded Bracelet";
+    };
+};
+
+function ring(){
+    if(document.getElementById("m").innerHTML === "Amethyst Black Gold Ring"){
+        document.getElementById("m").innerHTML = "#50,000.00 (Fifty Thousand Naira)";
+    } else {
+        document.getElementById("m").innerHTML = "Amethyst Black Gold Ring";
+    };
+};
+
+function purse(){
+    if(document.getElementById("n").innerHTML === "Quilted Drawstring Purse"){
+        document.getElementById("n").innerHTML = "#30,000.00 (Thirty Thousand Naira)";
+    } else {
+        document.getElementById("n").innerHTML = "Quilted Drawstring Purse";
+    };
+};
+
+function bag(){
+    if(document.getElementById("o").innerHTML === "Blushing Pink Bag"){
+        document.getElementById("o").innerHTML = "#45,000.00 (Forty-Five Thousand Naira)";
+    } else {
+        document.getElementById("o").innerHTML = "Blushing Pink Bag";
+    };
+};
+
+function facet(){
+    if(document.getElementById("p").innerHTML === "Beaded Royal Fascinator"){
+        document.getElementById("p").innerHTML = "#35,000.00 (Thirty-Five Thousand Naira)";
+    } else {
+        document.getElementById("p").innerHTML = "Beaded Royal Fascinator";
+    };
+};
+
+function bangle(){
+    if(document.getElementById("q").innerHTML === "Exclusive Bridal Bangle"){
+        document.getElementById("q").innerHTML = "#60,000.00 (Sixty Thousand Naira)";
+    } else {
+        document.getElementById("q").innerHTML = "Exclusive Bridal Bangle";
+    };
+};
+
+function shade(){
+    if(document.getElementById("r").innerHTML === "Fashion Tea Sunglass"){
+        document.getElementById("r").innerHTML = "#25,000.00 (Twenty-Five Thousand Naira)";
+    } else {
+        document.getElementById("r").innerHTML = "Fashion Tea Sunglass";
+    };
+};
+
+function crystal(){
+    if(document.getElementById("u").innerHTML === "Crystal Stone Jewelry"){
+        document.getElementById("u").innerHTML = "#70,000.00 (Seventy Thousand Naira)";
+    } else {
+        document.getElementById("u").innerHTML = "Crystal Stone Jewelry";
+    };
+};
+
+function stone(){
+    if(document.getElementById("s").innerHTML === "Crystal Stone Earring"){
+        document.getElementById("s").innerHTML = "#45,000.00 (Forty-Five Thousand Naira)";
+    } else {
+        document.getElementById("s").innerHTML = "Crystal Stone Earring";
+    };
+};
+ 
+function pearl(){
+    if(document.getElementById("w").innerHTML === "Fresh Water Pearl Bracelet"){
+        document.getElementById("w").innerHTML = "#45,000.00 (Forty-Five Thousand Naira)";
+    } else {
+        document.getElementById("w").innerHTML = "Fresh Water Pearl Bracelet";
+    };
+};
+
+function pattern(){
+    if(document.getElementById("v").innerHTML === "Floral Pattern Scarf"){
+        document.getElementById("v").innerHTML = "#15,000.00 (Fifteen Thousand Naira)";
+    } else {
+        document.getElementById("v").innerHTML = "Floral Pattern Scarf";
+    };
+};
+
+function cross(){
+    if(document.getElementById("x").innerHTML === "Golden Cross Chain"){
+        document.getElementById("x").innerHTML = "#20,000.00 (Twenty Thousand Naira)";
+    } else {
+        document.getElementById("x").innerHTML = "Golden Cross Chain";
+    };
+};
+
+function lilac(){
+    if(document.getElementById("y").innerHTML === "Lilac Purse"){
+        document.getElementById("y").innerHTML = "#35,000.00 (Thirty-Five Thousand Naira)";
+    } else {
+        document.getElementById("y").innerHTML = "Lilac Purse";
+    };
+};
+
+function rose(){
+    if(document.getElementById("z").innerHTML === "Rose Gold Earring"){
+        document.getElementById("z").innerHTML = "#25,000.00 (Twenty-Five Thousand Naira)";
+    } else {
+        document.getElementById("z").innerHTML = "Rose Gold Earring";
+    };
+};
+
+function charm(){
+    if(document.getElementById("aa").innerHTML === "Silver Leaf Charm Bracelet"){
+        document.getElementById("aa").innerHTML = "#15,000.00 (Fifteen Thousand Naira)";
+    } else {
+        document.getElementById("aa").innerHTML = "Silver Leaf Charm Bracelet";
+    };
+};
+
+function waist(){
+    if(document.getElementById("ab").innerHTML === "Silver Rhinestone Waist Belt"){
+        document.getElementById("ab").innerHTML = "#30,000.00 (Thirty Thousand Naira)";
+    } else {
+        document.getElementById("ab").innerHTML = "Silver Rhinestone Waist Belt";
+    };
+};
+
+function glass(){
+    if(document.getElementById("ae").innerHTML === "Vintage Rhinestone Sunglass"){
+        document.getElementById("ae").innerHTML = "#25,000.00 (Twenty-Five Thousand Naira)";
+    } else {
+        document.getElementById("ae").innerHTML = "Vintage Rhinestone Sunglass";
+    };
+};
+
+function bride(){
+    if(document.getElementById("af").innerHTML === "Bridal Hat"){
+        document.getElementById("af").innerHTML = "#20,000.00 (Twwenty Thousand Naira)";
+    } else {
+        document.getElementById("af").innerHTML = "Bridal Hat";
+    };
+};
+
+function silk(){
+    if(document.getElementById("ag").innerHTML === "Black Silk Fascinator"){
+        document.getElementById("ag").innerHTML = "#15,000.00 (Fifteen Thousand Naira)";
+    } else {
+        document.getElementById("ag").innerHTML = "Black Silk Fascinator";
+    };
+};
+
+function flop(){
+    if(document.getElementById("ar").innerHTML === "High Bow Flops"){
+        document.getElementById("ar").innerHTML = "#10,000.00 (Ten Thousand Naira)";
+    } else {
+        document.getElementById("ar").innerHTML = "High Bow Flops";
+    };
+};
+
+function metal(){
+    if(document.getElementById("aq").innerHTML === "Metal Chain Sandal"){
+        document.getElementById("aq").innerHTML = "#10,000.00 (Ten Thousand Naira)";
+    } else {
+        document.getElementById("aq").innerHTML = "Metal Chain Sandal";
+    };
+};
+
+function classic(){
+    if(document.getElementById("an").innerHTML === "Classic Heel"){
+        document.getElementById("an").innerHTML = "#30,000.00 (Thirty Thousand Naira)";
+    } else {
+        document.getElementById("an").innerHTML = "Classic Heel";
+    };
+};
+
+function wedge(){
+    if(document.getElementById("ap").innerHTML === "Black Crossed Wedge"){
+        document.getElementById("ap").innerHTML = "#35,000.00 (Thirty-Five Thousand Naira)";
+    } else {
+        document.getElementById("ap").innerHTML = "Black Crossed Wedge";
+    };
+};
+
+function red(){
+    if(document.getElementById("ao").innerHTML === "Red Summer Heel"){
+        document.getElementById("ao").innerHTML = "#40,000.00 (Forty Thousand Naira)";
+    } else {
+        document.getElementById("ao").innerHTML = "Red Summer Heel";
+    };
+};
+
+function pump(){
+    if(document.getElementById("as").innerHTML === "Black Laced Heel"){
+        document.getElementById("as").innerHTML = "#35,000.00 (Thirty-Five Thousand Naira)";
+    } else {
+        document.getElementById("as").innerHTML = "Black Laced Heel";
+    };
+};
+
+function wrap(){
+    if(document.getElementById("at").innerHTML === "Ankle Wrap Sandal"){
+        document.getElementById("at").innerHTML = "#25,000.00 (Twenty-Five Thousand Naira)";
+    } else {
+        document.getElementById("at").innerHTML = "Ankle Wrap Sandal";
+    };
+};
+
+function bridal(){
+    if(document.getElementById("au").innerHTML === "Blue Bridal Heel"){
+        document.getElementById("au").innerHTML = "#40,000.00 (Forty Thousand Naira)";
+    } else {
+        document.getElementById("au").innerHTML = "Blue Bridal Heel";
+    };
+};
+
+function chunky(){
+    if(document.getElementById("av").innerHTML === "Chunky Snicker"){
+        document.getElementById("av").innerHTML = "#30,000.00 (Thirty Thousand Naira)";
+    } else {
+        document.getElementById("av").innerHTML = "Chunky Snicker";
+    };
+};
+
+function snow(){
+    if(document.getElementById("aw").innerHTML === "Fur Winter Boot"){
+        document.getElementById("aw").innerHTML = "#50,000.00 (Fifty Thousand Naira)";
+    } else {
+        document.getElementById("aw").innerHTML = "Fur Winter Boot";
+    };
+};
+
+function snicker(){
+    if(document.getElementById("ax").innerHTML === "Black Snicker"){
+        document.getElementById("ax").innerHTML = "#30,000.00 (Thirty Thousand Naira)";
+    } else {
+        document.getElementById("ax").innerHTML = "Black Snicker";
+    };
+};
+
+function flat(){
+    if(document.getElementById("ay").innerHTML === "Flat"){
+        document.getElementById("ay").innerHTML = "#25,000.00 (Twenty Five Thousand Naira)";
+    } else {
+        document.getElementById("ay").innerHTML = "Black Snicker";
+    };
+};
