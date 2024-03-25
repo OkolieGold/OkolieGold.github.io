59fbf67e2a470141ab6eac4cd7e647c89d2e3083
OkolieGold <139628934+OkolieGold@users.noreply.github.com>
2024-03-25 22:15:10 +0100

    Uploaded a new folder

---
+++ counting.js
@@ -0,0 +1,24 @@
+// // next = 0;
+// // function count(){
+// //     next = next + 5;
+// //     document.getElementById("number").innerHTML = next;
+// // }
+
+
+// const numberEls = document.querySelectorAll('.number')
+
+// numberEls.addEventListener('click', function(){
+//     console.log(this);
+// });
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+function print100(){
+    number = 1;
+    console.log(number++);
+    
+}
+
+print100();
